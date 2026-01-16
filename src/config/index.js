5f7b8d2e11594e3851625896284c14be27e31d7d
Oluwapelumi Kehinde <pelumiwork@gmail.com>
2026-01-16 20:07:58 +0100

    Fix: Sanitize PAYSTACK_SECRET_KEY to remove extra quotes

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 3000,
   DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL.replace(/['"]+/g, '') : undefined,
-  PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY,
+  PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY ? process.env.PAYSTACK_SECRET_KEY.replace(/['"]+/g, '') : undefined,
   PAYSTACK_PUBLIC_KEY: process.env.PAYSTACK_PUBLIC_KEY,
 };
